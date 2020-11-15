6f35f74cf83f54adf57ad176c271ad5b32341386
positiveko <edie_ko@naver.com>
2020-11-15 14:51:15 +0900

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,50 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import styles from './app.module.css';
+import SearchHeader from './components/Search_header/Search_header';
+import VideoDetail from './components/video_detail/Video_detail';
+import VideoList from './components/video_list/video_list';
+
+function App({ youtube }) {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
+  const search = useCallback((query) => {
+    setSelectedVideo(null);
+    youtube
+      .search(query) //
+      .then((videos) => {
+        setVideos(videos);
+      });
+  }, [youtube]);
+
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
+  }, [youtube]);
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search_header/Search_header.jsx
@@ -0,0 +1,44 @@
+import React, { memo, useRef } from 'react';
+import Styles from './search_header.module.css';
+
+const SearchHeader = memo(({ onSearch }) => {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+  const onClick = () => {
+    handleSearch();
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <header className={Styles.header}>
+      <div className={Styles.logo}>
+        <img className={Styles.img} src='/images/logo.png' alt='logo' />
+        <h1 className={Styles.title}>Youtube</h1>
+      </div>
+      <input
+        ref={inputRef}
+        className={Styles.input}
+        type='search'
+        placeholder='Search...'
+        onKeyPress={onKeyPress}
+      />
+      <button className={Styles.button} type='submit' onClick={onClick}>
+        <img
+          className={Styles.buttonImg}
+          src='/images/search.png'
+          alt='search'
+        />
+      </button>
+    </header>
+  );
+});
+
+export default SearchHeader;
